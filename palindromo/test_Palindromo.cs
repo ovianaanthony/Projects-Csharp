e0099f110354ae998fbad549450a867ecfe31dd9
ovianaanthony <anthony.pe.viana@gmail.com>
2024-06-20 18:05:37 -0300

    Update test_Palindromo.cs

---
+++ palindromo/test_Palindromo.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace palindromo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Palindromo.Show(125);
+            Palindromo.Show(121);
+            Palindromo.Show(-121);
+        }
+    }
+}
